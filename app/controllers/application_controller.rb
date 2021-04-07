fbf7ce0fc5b717a9246bd9b5a846af15af40861d
kajikentaro <k.kentarou0412@gmail.com>
2021-04-07 17:11:27 +0900

    add line login button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
     def login
+        state = SecureRandom.urlsafe_base64
+        client_id = 1655827877
+        redirect_uri = ERB::Util.url_encode("https://google.com");
+
+        @url = "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=#{client_id}&redirect_uri=#{redirect_uri}&state=#{state}&scope=profile%20openid"
         render 'login'
     end
 end
